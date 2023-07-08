3ef9d217b3f68e4ccc8947f096aac03374e84339
John Bell <johnrbell@gmail.com>
2023-07-08 12:14:18 -0400

    webserver configuration/initilization

--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,78 @@
 #include <Arduino.h>
 #include <SPIFFS.h>
 #include <WiFi.h>
+// #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
-// WiFi credentials
-const char *WIFI_SSID = "test_network";
-const char *WIFI_PASS = "espresso";
-  
+// SPIFFS initialization
+void initSPIFFS() {
+  if (!SPIFFS.begin()) {
+    Serial.println("Error mounting SPIFFS volume.");
+  }else{
+    Serial.println("Mounted SPIFFS volume successfully.");
+  }
+}
+
+// WiFi credentials & init
+const char *WIFI_SSID = "Fios-mVd9B";
+const char *WIFI_PASS = "had43oasis28jet";
+
+void initWiFi() {
+  Serial.println("WTF");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.println("WTF2");
+  Serial.printf("Trying to connect [%s] ", WiFi.macAddress().c_str());
+  Serial.println("WTF3");
+  while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(500);
+  }
+  Serial.println("WTF4");
+  Serial.printf(" %s\n", WiFi.localIP().toString().c_str());
+}
+
 // Web Server Setup
-#define HTTP_PORT 80
-AsyncWebServer server(HTTP_PORT);
+AsyncWebServer server(80);
+
+// Web Server Initilization
+String processor(const String &var) {
+    return String("testing");
+}
 
 void onRootRequest(AsyncWebServerRequest *request) {
+  Serial.println("/index requested.");
   request->send(SPIFFS, "/index.html", "text/html", false, processor);
 }
 
 void initWebServer() {
     server.on("/", onRootRequest);
+
+//     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+//         Serial.println("TESTING.");
+//         request->send(200, "text/plain", "Hello World");
+//     });
+
     server.serveStatic("/", SPIFFS, "/");
     server.begin();
 }
-String processor(const String &var) {
-    return String(var == "STATE" && led.on ? "on" : "off");
-}
 
 
-// put function declarations here:
-// int myFunction(int, int);
-
-// ----------------------------------------------------------------------------
-// SPIFFS initialization
-// ----------------------------------------------------------------------------
-void initSPIFFS() {
-  if (!SPIFFS.begin()) {
-    Serial.println("Cannot mount SPIFFS volume...");
-    // while(1) digitalWrite(LED_BUILTIN, millis() % 200 < 50 ? HIGH : LOW);
-  }
-}
-
-void initWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  Serial.printf("Trying to connect [%s] ", WiFi.macAddress().c_str());
-  while (WiFi.status() != WL_CONNECTED) {
-      Serial.print(".");
-      delay(500);
-  }
-  Serial.printf(" %s\n", WiFi.localIP().toString().c_str());
-}
-
+// Sketch Functions
 void setup() {
-  // put your setup code here, to run once:
-  // int result = myFunction(2, 3);
   pinMode(32, OUTPUT);
   
-  Serial.begin(9600);
+  Serial.begin(115200); delay(500);
   Serial.println("Started.");
 
   initSPIFFS();
   initWiFi();
+  delay(5000);
   initWebServer();
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(32, HIGH);
   delay(1000);
   digitalWrite(32, LOW);
   delay(500);
 }
-
-// put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
