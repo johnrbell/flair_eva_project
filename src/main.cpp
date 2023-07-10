1d28eecae92e6ef07ca9339c749028cb24f7485c
John Bell <johnrbell@gmail.com>
2023-07-09 22:03:54 -0400

    fix formatting in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -64,20 +64,20 @@ void onEvent(AsyncWebSocket       *server,
              AwsEventType          type,
              void                 *arg,
              uint8_t              *data,
-             size_t                len) {
-
-    switch (type) {
-        case WS_EVT_CONNECT:
-            Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-            break;
-        case WS_EVT_DISCONNECT:
-            Serial.printf("WebSocket client #%u disconnected\n", client->id());
-            break;
-        case WS_EVT_DATA:
-        case WS_EVT_PONG:
-        case WS_EVT_ERROR:
-            break;
-    }
+             size_t                len) 
+{
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
 }
 
 // WebSocket Init
