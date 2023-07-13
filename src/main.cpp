9db52beee284fe3e63111c70b52b4f7529a7abcb
John Bell <johnrbell@gmail.com>
2023-07-13 00:04:26 -0400

    define button object, debounce stuff

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,71 @@
 
 #define LED_PIN 32
 #define BTN_PIN 22
-const uint8_t DEBOUNCE_DELAY 10; 
+
+const uint8_t DEBOUNCE_DELAY = 10;
+
+// ----------------------------------------------------------------------------
+// Define Led
+// ----------------------------------------------------------------------------
+struct Led {
+    uint8_t pin;
+    bool    on;
+
+    void update() {
+        digitalWrite(pin, on ? HIGH : LOW);
+    }
+};
 
 
 // ----------------------------------------------------------------------------
-// Helpers
+// Define Button
 // ----------------------------------------------------------------------------
+struct Button {
+    uint8_t  pin;
+    bool     lastReading;
+    uint32_t lastDebounceTime;
+    uint16_t state;
+
+    // methods determining the logical state of the button
+    bool pressed()                { return state == 1; }
+    bool released()               { return state == 0xffff; }
+    bool held(uint16_t count = 0) { return state > 1 + count && state < 0xffff; }
+
+    // method for reading the physical state of the button
+    void read() {
+        // reads the voltage on the pin connected to the button
+        bool reading = digitalRead(pin);
+
+        // if the logic level has changed since the last reading...
+        // we reset the timer which counts down the necessary time
+        // beyond which we can consider that the bouncing effect
+        // has passed.
+        if (reading != lastReading) {
+            lastDebounceTime = millis();
+        }
+
+        // from the moment we're out of the bouncing phase...
+        // the actual status of the button can be determined
+        if (millis() - lastDebounceTime > DEBOUNCE_DELAY) {
+            // don't forget that the read pin is pulled-up
+            bool pressed = reading == LOW;
+            if (pressed) {
+                     if (state  < 0xfffe) state++;
+                else if (state == 0xfffe) state = 2;
+            } else if (state) {
+                state = state == 0xffff ? 0 : 0xffff;
+            }
+        }
+
+        // finally, each new reading is saved
+        lastReading = reading;
+    }
+};
+
+// ----------------------------------------------------------------------------
+// WebServer/Socket Helpers 
+// ----------------------------------------------------------------------------
+
 // Web Server & Socket Setup
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
